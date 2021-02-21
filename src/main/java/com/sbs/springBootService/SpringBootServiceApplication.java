39f631a6eaad81df35a0213dded9ef5623487d3a
ucong2222 <dbrudrjf21@gmail.com>
2021-02-22 01:00:51 +0900

    첫 커밋

---
+++ src/main/java/com/sbs/springBootService/SpringBootServiceApplication.java
@@ -0,0 +1,13 @@
+package com.sbs.springBootService;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringBootServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringBootServiceApplication.class, args);
+	}
+
+}
