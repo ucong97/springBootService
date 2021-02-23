9f83ada7ae00aac0cae71fcf97fbaf76894b1f76
ucong2222 <dbrudrjf21@gmail.com>
2021-02-24 00:11:25 +0900

    보고서 양식을 map에서 ResultData로 변경

--- src/main/java/com/sbs/springBootService/controller/UsrArticleController.java
+++ src/main/java/com/sbs/springBootService/controller/UsrArticleController.java
@@ -2,13 +2,13 @@ package com.sbs.springBootService.controller;
 
 import java.util.ArrayList;
 import java.util.List;
-import java.util.Map;
 
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.ResponseBody;
 
 import com.sbs.springBootService.dto.Article;
+import com.sbs.springBootService.dto.ResultData;
 import com.sbs.springBootService.util.Util;
 
 @Controller
@@ -40,25 +40,25 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/doAdd")
 	@ResponseBody
-	public Map<String, Object> doAdd(String title, String body) {
+	public ResultData doAdd(String title, String body) {
 		String regDate = Util.getNowDateStr();
 		String updateDate = regDate;
 
 		articles.add(new Article(++articlesLastId, regDate, updateDate, title, body));
 
-		return Util.mapOf("resultCode", "S-1", "msg", "성공", "id", articlesLastId);
+		return new ResultData("S-1", "성공", "id", articlesLastId);
 	}
 
 	@RequestMapping("/usr/article/doDelete")
 	@ResponseBody
-	public Map<String, Object> doDelete(int id) {
+	public ResultData doDelete(int id) {
 		boolean deleteArticleRs = deleteArticle(id);
 
-		if (deleteArticleRs==false) {
-			return Util.mapOf("resultCode", "F-1","msg", "해당 게시물은 존재하지 않습니다.");
+		if (deleteArticleRs == false) {
+			return new ResultData("F-1", "해당 게시물은 존재하지 않습니다.");
 		}
-		
-		return Util.mapOf("resultCode", "S-1","msg", "성공","id",id);
+
+		return new ResultData("S-1", "성공", "id", id);
 	}
 
 	private boolean deleteArticle(int id) {
@@ -73,7 +73,7 @@ public class UsrArticleController {
 
 	@RequestMapping("/usr/article/doModify")
 	@ResponseBody
-	public Map<String, Object> doModify(int id, String title, String body) {
+	public ResultData doModify(int id, String title, String body) {
 		Article selArticle = null;
 
 		for (Article article : articles) {
@@ -84,14 +84,14 @@ public class UsrArticleController {
 		}
 
 		if (selArticle == null) {
-			return Util.mapOf("resultCode", "F-1","msg", String.format("%d번 게시물은 존재하지 않습니다.", id));
+			return new ResultData("F-1", String.format("%d번 게시물은 존재하지 않습니다.", id));
 		}
 
 		selArticle.setUpdateDate(Util.getNowDateStr());
 		selArticle.setTitle(title);
 		selArticle.setBody(body);
 
-		return Util.mapOf("resultCode", "S-1","msg", String.format("%d번 게시물이 수정되었습니다.", id),"id", id);
+		return new ResultData("S-1", String.format("%d번 게시물이 수정되었습니다.", id), "id", id);
 
 	}
 
