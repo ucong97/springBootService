a9055535e1329fb1787936946b658bd598d6f04c
ucong2222 <dbrudrjf21@gmail.com>
2021-02-24 01:29:05 +0900

    게시물Dao 도입

--- src/main/java/com/sbs/springBootService/service/ArticleService.java
+++ src/main/java/com/sbs/springBootService/service/ArticleService.java
@@ -1,96 +1,45 @@
 package com.sbs.springBootService.service;
 
-import java.util.ArrayList;
 import java.util.List;
 
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 
+import com.sbs.springBootService.dao.ArticleDao;
 import com.sbs.springBootService.dto.Article;
 import com.sbs.springBootService.dto.ResultData;
-import com.sbs.springBootService.util.Util;
 
 @Service // Conponent
 public class ArticleService {
-	private int articlesLastId;
-	private List<Article> articles;
-
-	public ArticleService() {
-		// 멤버변수 초기화
-		articlesLastId = 0;
-		articles = new ArrayList<>();
-
-		// 게시물 2개 생성
-		articles.add(
-				new Article(++articlesLastId, "2020-12-12 12:12:12", "2020-12-12 12:12:12", "제목1 입니다.", "내용1 입니다."));
-		articles.add(
-				new Article(++articlesLastId, "2020-12-12 12:12:12", "2020-12-12 12:12:12", "제목2 입니다.", "내용2 입니다."));
-	}
+	@Autowired
+	private ArticleDao articleDao;
 
 	public Article getArticle(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				return article;
-			}
-		}
-		return null;
+		return articleDao.getArticle(id);
 	}
 
 	public List<Article> getArticles(String searchKeywordType, String searchKeyword) {
-		if (searchKeyword == null) {
-			return articles;
-		}
-
-		List<Article> filtered = new ArrayList<>();
-
-		for (Article article : articles) {
-			boolean contains = false;
-
-			if (searchKeywordType.equals("title")) {
-				contains = article.getTitle().contains(searchKeyword);
-			} else if (searchKeywordType.equals("body")) {
-				contains = article.getBody().contains(searchKeyword);
-			} else {
-				contains = article.getTitle().contains(searchKeyword);
-
-				if (contains == false) {
-					contains = article.getBody().contains(searchKeyword);
-				}
-
-			}
-
-			if (contains) {
-				filtered.add(article);
-			}
-		}
-		return filtered;
+		return articleDao.getArticles(searchKeywordType, searchKeyword);
 	}
 
-	public ResultData add(String title, String body) {
-		int id = ++articlesLastId;
-		String regDate = Util.getNowDateStr();
-		String updateDate = regDate;
-
-		articles.add(new Article(id, regDate, updateDate, title, body));
+	public ResultData addArticle(String title, String body) {
+		int id = articleDao.addArticle(title, body);
 
 		return new ResultData("S-1", "성공", "id", id);
 	}
 
 	public ResultData deleteArticle(int id) {
-		for (Article article : articles) {
-			if (article.getId() == id) {
-				articles.remove(article);
-				return new ResultData("S-1", "삭제하였습니다.", "id", id);
-			}
+		boolean rs = articleDao.deleteArticle(id);
+
+		if (rs == false) {
+			return new ResultData("F-1", "해당 게시물은 존재하지 않습니다.", "id", id);
 		}
-		return new ResultData("F-1", "해당 게시물은 존재하지 않습니다.", "id", id);
+
+		return new ResultData("S-1", "삭제하였습니다.", "id", id);
 	}
 
-	public ResultData modify(int id, String title, String body) {
-		Article article = getArticle(id);
-
-		article.setTitle(title);
-		article.setBody(body);
-		article.setUpdateDate(Util.getNowDateStr());
+	public ResultData modifyArticle(int id, String title, String body) {
+		articleDao.modifyArticle(id, title, body);
 
 		return new ResultData("S-1", "게시물을 수정하였습니다.", "id", id);
 	}
