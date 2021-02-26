46e96824e18fc9eecff89a4141fc166c5575c041
ucong2222 <dbrudrjf21@gmail.com>
2021-02-26 16:10:08 +0900

    게시물 데이터 테스트 데이터 개수 늘리는 SQL을 db.sql에 추가

--- db.sql
+++ db.sql
@@ -84,3 +84,13 @@ ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDa
 UPDATE article
 SET memberId = 1
 WHERE memberId=0;
+
+/*
+insert into article
+(regDate, updateDate, memberId, title, `body`)
+SELECT NOW(), NOW(), FLOOR(RAND() * 2) + 1, CONCAT('제목_', FLOOR(RAND() * 1000) + 1), CONCAT('내용_', FLOOR(RAND() * 1000) + 1)
+from article;
+
+SELECT COUNT(*) FROM article; 
+*/
+
