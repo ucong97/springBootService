1d7c0de424ab42ba7b03aa828fe2441f499f9d88
ucong2222 <dbrudrjf21@gmail.com>
2021-02-26 11:47:21 +0900

    기존 게시물의 작성자를 회원1로 지정

--- db.sql
+++ db.sql
@@ -69,3 +69,8 @@ email = "dbrudrjf21@gmail.com";
 
 # 게시물 테이블에 회원번호 칼럼 추가
 ALTER TABLE article ADD COLUMN memberId INT(10) UNSIGNED NOT NULL AFTER updateDate;
+
+# 기존 게시물의 작성자를 회원1로 지정
+UPDATE article
+SET memberId = 1
+WHERE memberId=0;
