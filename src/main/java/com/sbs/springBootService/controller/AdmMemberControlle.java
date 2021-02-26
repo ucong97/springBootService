5b9c0ea5c5e9b3efec3ae9fe19ef0dcde75d0fd1
ucong2222 <dbrudrjf21@gmail.com>
2021-02-26 20:59:15 +0900

    관리자 페이지 로그인 화면 구현

--- src/main/java/com/sbs/springBootService/controller/AdmMemberControlle.java
+++ src/main/java/com/sbs/springBootService/controller/AdmMemberControlle.java
@@ -20,6 +20,11 @@ public class AdmMemberControlle {
 	@Autowired
 	private MemberService memberService;
 
+	@RequestMapping("/adm/member/login")
+	public String login() {
+		return "adm/member/login";
+	}
+
 	@RequestMapping("/adm/member/doLogin")
 	@ResponseBody
 	public ResultData doLogin(String loginId, String loginPw, HttpSession session) {
@@ -42,10 +47,10 @@ public class AdmMemberControlle {
 			return new ResultData("F-3", "비밀번호가 일치하지 않습니다.");
 		}
 
-		if(memberService.isAdmin(existingMember) == false) {
+		if (memberService.isAdmin(existingMember) == false) {
 			return new ResultData("F-4", "관리자만 접근할 수 있는 페이지 입니다.");
 		}
-		
+
 		session.setAttribute("loginedMemberId", existingMember.getId());
 
 		return new ResultData("S-1", String.format("%s 님 환영합니다.", existingMember.getNickname()));
