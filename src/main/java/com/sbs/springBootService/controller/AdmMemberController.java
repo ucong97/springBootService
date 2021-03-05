83285b992abe08f670a24f695df49abf59d89daa
ucong2222 <dbrudrjf21@gmail.com>
2021-03-05 19:58:33 +0900

    로그인 처리시 관리자 홈으로 이동

--- src/main/java/com/sbs/springBootService/controller/AdmMemberController.java
+++ src/main/java/com/sbs/springBootService/controller/AdmMemberController.java
@@ -14,9 +14,10 @@ import org.springframework.web.bind.annotation.ResponseBody;
 import com.sbs.springBootService.dto.Member;
 import com.sbs.springBootService.dto.ResultData;
 import com.sbs.springBootService.service.MemberService;
+import com.sbs.springBootService.util.Util;
 
 @Controller
-public class AdmMemberControlle {
+public class AdmMemberController {
 	@Autowired
 	private MemberService memberService;
 
@@ -27,33 +28,35 @@ public class AdmMemberControlle {
 
 	@RequestMapping("/adm/member/doLogin")
 	@ResponseBody
-	public ResultData doLogin(String loginId, String loginPw, HttpSession session) {
+	public String doLogin(String loginId, String loginPw, HttpSession session) {
 
 		if (loginId == null) {
-			return new ResultData("F-1", "loginId를 입력해주세요.");
+			return Util.msgAndBack("loginId를 입력해주세요.");
 		}
 
 		Member existingMember = memberService.getMemberByLoginId((String) loginId);
 
 		if (existingMember == null) {
-			return new ResultData("F-2", "존재하지 않는 로그인아이디 입니다.", "loginId", loginId);
+			return Util.msgAndBack("존재하지 않는 로그인아이디 입니다.");
 		}
 
 		if (loginPw == null) {
-			return new ResultData("F-1", "loginPw를 입력해주세요.");
+			return Util.msgAndBack("loginPw를 입력해주세요.");
 		}
 
 		if (existingMember.getLoginPw().equals(loginPw) == false) {
-			return new ResultData("F-3", "비밀번호가 일치하지 않습니다.");
+			return Util.msgAndBack("비밀번호가 일치하지 않습니다.");
 		}
 
 		if (memberService.isAdmin(existingMember) == false) {
-			return new ResultData("F-4", "관리자만 접근할 수 있는 페이지 입니다.");
+			return Util.msgAndBack("관리자만 접근할 수 있는 페이지 입니다.");
 		}
 
 		session.setAttribute("loginedMemberId", existingMember.getId());
 
-		return new ResultData("S-1", String.format("%s 님 환영합니다.", existingMember.getNickname()));
+		String msg = String.format("%s 님 환영합니다.", existingMember.getNickname());
+
+		return Util.msgAndReplace(msg, "../home/main");
 	}
 
 	@RequestMapping("/adm/member/doModify")
