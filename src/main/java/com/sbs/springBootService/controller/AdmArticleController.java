6aa291f92c9f8e75d093a62f2bc750c0843f1a75
ucong2222 <dbrudrjf21@gmail.com>
2021-03-07 01:16:08 +0900

    관리자 게시물 리스트

--- src/main/java/com/sbs/springBootService/controller/AdmArticleController.java
+++ src/main/java/com/sbs/springBootService/controller/AdmArticleController.java
@@ -17,7 +17,7 @@ import com.sbs.springBootService.dto.ResultData;
 import com.sbs.springBootService.service.ArticleService;
 
 @Controller
-public class AdmArticleController {
+public class AdmArticleController extends BaseController{
 	@Autowired
 	private ArticleService articleService;
 
@@ -38,14 +38,12 @@ public class AdmArticleController {
 	}
 
 	@RequestMapping("/adm/article/list")
-	@ResponseBody
-	public ResultData showList(@RequestParam(defaultValue = "1") int page,
-			@RequestParam(defaultValue = "1") int boardId, String searchKeywordType, String searchKeyword) {
+	public String showList(HttpServletRequest req, @RequestParam(defaultValue = "1") int boardId, String searchKeywordType, String searchKeyword, @RequestParam(defaultValue = "1") int page) {
 		// @RequestParam(defaultValue = "titleAndTitle") String searchKeywordType
 		Board board = articleService.getBoard(boardId);
 
 		if (board == null) {
-			return new ResultData("F-1", "존재하지 않는 게시판입니다.");
+			return msgAndBack(req, "존재하지 않는 게시판입니다.");
 		}
 
 		if (searchKeywordType != null) {
@@ -73,7 +71,9 @@ public class AdmArticleController {
 		List<Article> articles = articleService.getForPrintArticles(boardId, searchKeywordType, searchKeyword, page,
 				itemsInAPage);
 
-		return new ResultData("S-1", "성공", "articles", articles);
+		req.setAttribute("articles", articles);
+		
+		return "adm/article/list";
 	}
 
 	@RequestMapping("/adm/article/doAdd")
