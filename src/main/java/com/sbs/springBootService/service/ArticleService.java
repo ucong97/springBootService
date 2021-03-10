92d23eebee63a6f274198e195f4e1cadbfc3b1b9
ucong2222 <dbrudrjf21@gmail.com>
2021-03-11 00:22:45 +0900

    게시물 수정시 첨부파일 덮어쓰기

--- src/main/java/com/sbs/springBootService/service/ArticleService.java
+++ src/main/java/com/sbs/springBootService/service/ArticleService.java
@@ -42,7 +42,7 @@ public class ArticleService {
 	public ResultData deleteArticle(int id) {
 		articleDao.deleteArticle(id);
 		
-		genFileService.deleteFiles("article", id);
+		genFileService.deleteGenFiles("article", id);
 		
 		return new ResultData("S-1", "삭제하였습니다.", "id", id);
 	}
