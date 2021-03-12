5b8507d2c450e349a9f0e8c001d224359f2c35ef
ucong2222 <dbrudrjf21@gmail.com>
2021-03-12 19:31:31 +0900

    파일 다운로드 내부 로직 수정시 파일명 깨지는 오류 수정

--- src/main/java/com/sbs/springBootService/controller/CommonGenFileController.java
+++ src/main/java/com/sbs/springBootService/controller/CommonGenFileController.java
@@ -3,6 +3,7 @@ package com.sbs.springBootService.controller;
 import java.io.File;
 import java.io.FileInputStream;
 import java.io.IOException;
+import java.net.URLEncoder;
 import java.util.Map;
 
 import javax.servlet.http.HttpServletRequest;
@@ -40,10 +41,12 @@ public class CommonGenFileController extends BaseController {
 	}
 	
 	@GetMapping("/common/genFile/doDownload")
-	public ResponseEntity<Resource> downloadFile(int id, HttpServletRequest request) throws IOException {
+	public ResponseEntity<Resource> downloadFile(int id, HttpServletRequest request) throws Exception {
 		GenFile genFile = genFileService.getGenFile(id);
 		String filePath = genFile.getFilePath(genFileDirPath);
 
+		String browser = "Chrome";
+		String filename = getEncodedFileName(genFile.getOriginFileName(), browser);
 		Resource resource = new InputStreamResource(new FileInputStream(filePath));
 
 		// Try to determine file's content type
@@ -56,8 +59,43 @@ public class CommonGenFileController extends BaseController {
 
 		return ResponseEntity
 				.ok()
-				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + genFile.getOriginFileName() + "\"")
+				.header(HttpHeaders.CONTENT_DISPOSITION, "attachment; filename=\"" + filename + "\"")
 				.contentType(MediaType.parseMediaType(contentType))
 				.body(resource);
 	}
+	
+	public static String getEncodedFileName(String filename, String browser) throws Exception {
+        
+        String encodedFilename = null;
+        if (browser.equals("MSIE")) {
+            // 한글 파일명 깨짐현상을 해결하기 위해 URLEncoder.encode 메소드를 사용하는데,
+            // 파일명에 공백이 존재할 경우 URLEncoder.encode 메소드에의해 공백이 '+' 로 변환됩니다.
+            // 변환된 '+' 값을 다시 공백으로(%20)으로 replace처리하시면 됩니다.
+            // \\+의 의미는 정규표현식에서 역슬래시(\)는 확장문자로
+            // 역슬래시 다음에 일반문자가 오면 특수문자로 취급하고 
+            // 역슬래시 다음에 특수문자가 오면 그 문자 자체를 의미 
+            // 기존 파일명에 있던 '+' 는 URLEncoder.encode() 메소드에 의해 '%2B' 로 변환이 됩니다.
+             encodedFilename = URLEncoder.encode(filename, "UTF-8").replaceAll("\\+", "%20");
+        } else if (browser.equals("Firefox")) {
+             encodedFilename = "\"" + new String(filename.getBytes("UTF-8"), "8859_1") + "\"";
+        } else if (browser.equals("Opera")) {
+             encodedFilename = "\"" + new String(filename.getBytes("UTF-8"), "8859_1") + "\"";
+        } else if (browser.equals("Chrome")) {
+           StringBuffer sb = new StringBuffer();
+           for (int i = 0; i < filename.length(); i++) {
+                       char c = filename.charAt(i);
+                       if (c > '~') {
+                                  sb.append(URLEncoder.encode("" + c, "UTF-8"));
+                       } else {
+                             // ASCII문자(0X00 ~ 0X7E)는 URLEncoder.encode를 적용하지 않는다.     
+                               sb.append(c);
+                       }
+           }
+           encodedFilename = sb.toString();
+        } else {
+           throw new RuntimeException("Not supported browser");
+        }
+        return encodedFilename;
+	}
+
 }
