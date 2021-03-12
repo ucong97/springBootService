be20417f591b53ad1441ca2113d8bd34db2b8d24
ucong2222 <dbrudrjf21@gmail.com>
2021-03-13 00:12:19 +0900

    changeInputFileRelIds 메서드를 genFileService로 옮기고 그것을 가입처리 할때도 적용

--- src/main/java/com/sbs/springBootService/service/MemberService.java
+++ src/main/java/com/sbs/springBootService/service/MemberService.java
@@ -15,6 +15,8 @@ import com.sbs.springBootService.util.Util;
 public class MemberService {
 	@Autowired
 	private MemberDao memberDao;
+	@Autowired
+	private GenFileService genFileService;
 	
 	// static 시작
 	
@@ -44,6 +46,8 @@ public class MemberService {
 		memberDao.join(param);
 
 		int id = Util.getAsInt(param.get("id"), 0);
+		
+		genFileService.changeInputFileRelIds(param, id);
 
 		return new ResultData("S-1", String.format("%s님 환영합니다.", param.get("nickname")), "id", id);
 	}
